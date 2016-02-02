fe93f02e9a4e7a0a25363c7519c64c0d4e8ff86f
tfleischauer <tfleischauer@gmail.com>
2016-02-02 13:55:56 -0800

    indented code

--- footer.php
+++ footer.php
@@ -7,27 +7,27 @@
         </div> <!-- end .col-md-12 -->
 	</div> <!-- end .row -->
 
-<div class="row footer">
-	<div class="footer col-md-12">
-		<div class="f_logo col-md-2" style="padding-top: 10px;">
-        	<img src="<?php echo get_bloginfo('template_directory');?>/images/logo-yoga-elements-140x56px.jpg" alt="YOGA Elements Studio" title="YOGA Elements Studio" width="140" height="56" />
-        </div>
-		<div class="col-md-6 footer-menu" style="padding-top: 10px;">
-  			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => '' ) ); ?>
-  		</div>
-		<div class="address col-md-4" style="padding-top: 10px;">
-            <h4>yoga elements studio - </h4>
-            <p> Behind Central Chidlom department store on Soi Chidlom<br />
-              In the Vanissa Building, 29 Soi Chidlom, 23rd Floor <br />
-              BTS: Chidlom</p>
-  		</div> <!-- end .address .col-md-4 -->
-	</div> <!-- end .footer .col-md-12 -->
-</div> <!-- end .row .footer -->
+    <div class="row footer">
+        <div class="footer col-md-12">
+            <div class="f_logo col-md-2" style="padding-top: 10px;">
+                <img src="<?php echo get_bloginfo('template_directory');?>/images/logo-yoga-elements-140x56px.jpg" alt="YOGA Elements Studio" title="YOGA Elements Studio" width="140" height="56" />
+            </div>
+            <div class="col-md-6 footer-menu" style="padding-top: 10px;">
+                <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => '' ) ); ?>
+            </div>
+            <div class="address col-md-4" style="padding-top: 10px;">
+                <h4>yoga elements studio - </h4>
+                <p> Behind Central Chidlom department store on Soi Chidlom<br />
+                  In the Vanissa Building, 29 Soi Chidlom, 23rd Floor <br />
+                  BTS: Chidlom</p>
+            </div> <!-- end .address .col-md-4 -->
+        </div> <!-- end .footer .col-md-12 -->
+    </div> <!-- end .row .footer -->
+    
+    <div class="call_now">Call to reserve your space <img src="<?php echo get_bloginfo('template_directory');?>/images/phone.gif" alt="" width="12" height="27" class="textmiddle" /><span class="number">02655-5671</span>
+    </div> <!-- end .call_now -->
 
-<div class="call_now">Call to reserve your space <img src="<?php echo get_bloginfo('template_directory');?>/images/phone.gif" alt="" width="12" height="27" class="textmiddle" /><span class="number">02655-5671</span>
-</div> <!-- end .call_now -->
-
-</div><!--end container-->
+  </div><!--end .container .wrapper -->
 <?php
     wp_footer();
 ?>
--- header.php
+++ header.php
@@ -42,55 +42,59 @@
 </head>
 
 <body>
-	<div class="container wrapper">
-      <div class="header row">
+  <div class="container wrapper">
+    <div class="header row">
 
-          <div class="logo col-md-3 col-sm-3">
-              <a href="<?php echo get_home_url(); ?>">
-                  <img src="<?php echo get_template_directory_uri(); ?>/images/logo-yoga-elements-200x80px.jpg" alt="YOGA Elements" title="YOGA Elements" width="200" height="80" />
-              </a>
-          </div>
+        <div class="logo col-md-3 col-sm-3">
+            <a href="<?php echo get_home_url(); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo-yoga-elements-200x80px.jpg" alt="YOGA Elements" title="YOGA Elements" width="200" height="80" />
+            </a>
+        </div>
 
-          <div class="col-md-5 col-sm-4"></div>
-          <div class="col-md-4 col-sm-5">
+        <div class="col-md-5 col-sm-4"></div>
+        <div class="col-md-4 col-sm-5">
 
-              <div class="icon">
-                  <a href="https://www.facebook.com/YogaElementsStudio" target="_blank">
-                      <img src="<?php echo get_template_directory_uri(); ?>/images/icon.gif" alt="Facebook" title="Facebook" width="32" height="30" />
-                  </a>
-              </div> <!-- end .icon -->
+            <div class="icon">
+                <a href="https://www.facebook.com/YogaElementsStudio" target="_blank">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon.gif" alt="Facebook" title="Facebook" width="32" height="30" />
+                </a>
+            </div> <!-- end .icon -->
 
-              <div class="top_nav">
-                  <a href="./newsletter" title="Sign up for Newsletter">
-                      <img src="<?php echo get_template_directory_uri(); ?>/images/nl.gif" alt="Newsletter" title="Newsletter" width="41" height="30" />
-                          <span>Sign up for Newsletter</span>
-                  </a>
-              </div> <!-- end .top_nav -->
-          </div> <!-- end .col-md-4 .col-sm-5 -->
-	</div> <!-- end .container .wrapper -->
+            <div class="top_nav">
+                <a href="./newsletter" title="Sign up for Newsletter">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/nl.gif" alt="Newsletter" title="Newsletter" width="41" height="30" />
+                        <span>Sign up for Newsletter</span>
+                </a>
+            </div> <!-- end .top_nav -->
+        </div> <!-- end .col-md-4 .col-sm-5 -->
+      </div> <!-- end .header .row -->
 
-    <div class="row" style="margin-bottom: 10px;">
-        <div class="col-md-12" style="padding: 0;">
-                <div class="main_nav">
-                  <div id="navigation" class="smartmenu navbar-default">
+  <div class="row" style="margin-bottom: 10px;">
+      <div class="col-md-12" style="padding: 0;">
+              <div class="main_nav">
+                <div id="navigation" class="smartmenu navbar-default">
 
-                      <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                        </button>
-                       </div> <!-- end .navbar-header -->
+                    <div class="navbar-header">
+                      <button type="button" 
+                      	class="navbar-toggle collapsed" 
+                        data-toggle="collapse" 
+                        data-target="#bs-example-navbar-collapse-1"
+                        aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                     </div> <!-- end .navbar-header -->
 
-                      <div class="collapse navbar-collapse in" id="bs-example-navbar-collapse-1">
-                          <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => '' ) ); ?>
-                      </div> <!-- end .collapse .navbar-collapse .in -->
+                    <div class="collapse navbar-collapse in" id="bs-example-navbar-collapse-1">
+                        <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => '' ) ); ?>
+                    </div> <!-- end .collapse .navbar-collapse .in -->
 
-                    </div> <!-- end #navigation -->
-                </div> <!-- end .main-nav -->
-        </div> <!-- end .col-md-12 -->
-    </div> <!-- end .row -->
+                  </div> <!-- end #navigation -->
+              </div> <!-- end .main-nav -->
+      </div> <!-- end .col-md-12 -->
+  </div> <!-- end .row -->
 
 <script type="text/javascript">
 	jQuery(document).ready(function(){
